49b28074c34ce82d5d8712ec12b78227913529db
Chinedum <sam.chinedum@gmail.com>
2023-07-03 05:43:21 -0700

    Scatter

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,18 +5,12 @@
  * Return: always returns 0 if code runs successful
  */
 
-int main(void)
-{
-	char a;
-	int b;
-	long int c
-	long long int d;
-	float e;
+int main() {
+    printf("Size of char: %zu byte(s)\n", sizeof(char));
+    printf("Size of int: %zu byte(s)\n", sizeof(int));
+    printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+    printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+    printf("Size of float: %zu byte(s)\n", sizeof(float));
 
-	printf("Size of char: %zu byte(s)\n", sizeof(a));
-	printf("Size of int: %zu byte(s)\n", sizeof(b));
-	printf("Size of long int: %zu byte(s)\n", sizeof(c));
-	printf("Size of long long int: %zu byte(s)\n", sizeof(d));
-	printf("Size of float: %zu byte(s)\n", sizeof(e));
-	return (0);
-}
+    return 0;
+
