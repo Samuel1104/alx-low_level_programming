372b55f856f3256ffbd62e168355883cf9129585
Chinedum <sam.chinedum@gmail.com>
2023-07-03 08:14:20 -0700

    Holba

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-#include <unistd.h>
 /**
- * main - main function
- *
- * Return: 1
+ * main - Entry point
+ * void - Empty
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	int n;
+
+	srand(time(0));
+	n = rand() -RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{	printf("%d is negtive\n" n);
+	}
+	return (0);
 }
