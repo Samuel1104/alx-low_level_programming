6fb18187b1b4e7eb62dfe7ccdd2377bee5d96a45
Chinedum <sam.chinedum@gmail.com>
2023-07-03 04:47:54 -0700

    Printfc

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function that starts the code
+ *
+ * Return: always 0 if the code is successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
